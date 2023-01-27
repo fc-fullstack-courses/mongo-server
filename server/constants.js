aa8f63c711cc2dbb3503b75a5380f13c6b225e46
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-27 20:24:06 +0200

    Add password hashing

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,7 @@
 const CONSTANTS = {
   PORT: process.env.PORT || 5000,
-  DB_STRING : process.env.DB_STRING || 'mongodb://localhost:27017/test'
+  DB_STRING : process.env.DB_STRING || 'mongodb://localhost:27017/test',
+  SALT_ROUNDS: 10
 }
 
 module.exports = CONSTANTS;
--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,31 +1,40 @@
 const yup = require('yup');
+const bcrypt = require('bcrypt');
 const { Schema, model } = require('../index');
+const { SALT_ROUNDS } = require('../../constants');
 
 const emailSchema = yup.string().email().required();
 
-const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: String,
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true,
-    validate: {
-      validator: (value) => emailSchema.isValid(value)
-    }
-   },
-  isMale: Boolean,
-  password: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (value) => emailSchema.isValid(value),
+      },
+    },
+    isMale: Boolean,
+    password: {
+      type: String,
+      required: true,
+      set: (password) => {
+        const passwordHash = bcrypt.hashSync(password, SALT_ROUNDS);
+        return passwordHash;
+      }
+    },
+    birthday: {
+      type: Date,
+    },
+    contacts: {
+      phone: { type: String },
+    },
   },
-  birthday: {
-    type: Date,
-  },
-  contacts: {
-    phone: { type: String },
-  },
-}, {timestamps: true });
+  { timestamps: true }
+);
 
 const User = model('User', userSchema);
 
