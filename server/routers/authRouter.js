59421a457a38037f70b141fc85f69a9f92341b5f
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-30 20:23:32 +0200

    Add validation on some routes

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,8 +1,9 @@
 const authRouter = require('express').Router();
 const AuthController = require('../controllers/authController');
+const { checkRefreshToken } = require('../middlewares/token.mw');
 
 authRouter.post('/registration', AuthController.registration);
 authRouter.post('/login', AuthController.login);
-authRouter.post('/refresh', AuthController.refresh);
+authRouter.post('/refresh', checkRefreshToken, AuthController.refresh);
 
 module.exports = authRouter;
