03ab140389b110797792d12cb2bb2384724a7b1d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-30 18:16:48 +0200

    Connect authRouter

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,8 @@
 const rootRouter = require('express').Router();
+const authRouter = require('./authRouter');
 const usersRouter = require('./usersRouter');
 
 rootRouter.use('/users', usersRouter);
+rootRouter.use('/auth', authRouter);
 
 module.exports = rootRouter;
