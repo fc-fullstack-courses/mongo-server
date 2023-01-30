08bbc0eeda3362693af34f880cd85b02b5e31b16
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-30 21:15:19 +0200

    Add greeting

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../../components/Header';
 
 function HomePage() {
+  const { data: user } = useSelector((state) => state.user);
   return (
     <div>
       <Header />
       HomePage
+      {user && <p>Hello {user.firstName} {user.lastName}</p>}
     </div>
   );
 }
