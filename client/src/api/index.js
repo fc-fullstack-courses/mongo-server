305eb8170ee43a228d242fd34ba05b1419738903
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-31 18:53:55 +0200

    Add interceptors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,6 +5,68 @@ const httpClient = axios.create({
   baseURL: CONSTANTS.HTTP_SERVER_URL,
 });
 
+let accessToken = null;
+
+httpClient.interceptors.response.use(
+  function (response) {
+    if (response?.data?.data?.tokens) {
+      // ответ был с токенами
+      // localStorage.setItem('token', response.data.data.tokens.refresh);
+      localStorage.setItem('token', response.data.data.tokens.access);
+      // access в инкапсуляции
+      accessToken = response.data.data.tokens.access;
+    }
+
+    return response;
+  },
+  async function (error) {
+    // токен протух
+
+    const {
+      response: { status },
+    } = error;
+
+    const refreshFromLS = localStorage.getItem('token');
+
+    if (status === 419 && refreshFromLS) {
+      const {
+        data: {
+          data: {
+            tokens: { access },
+          },
+        },
+      } = await axios.post(`${CONSTANTS.HTTP_SERVER_URL}/auth/refresh`, {
+        token: refreshFromLS,
+      });
+
+      // обновляем нашу токены
+      localStorage.setItem('token', access);
+      accessToken = access;
+
+      // устанавливаю заголовок
+      error.config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+      // перезапускаю запрос
+      return httpClient.request(error.config);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+httpClient.interceptors.request.use(
+  function (config) {
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  function (err) {
+    return Promise.reject(err);
+  }
+);
+
 export const login = (userData) => httpClient.post('/auth/login', userData);
 export const registration = (userData) =>
   httpClient.post('/auth/registration', userData);
